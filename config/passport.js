2764bd1ec3db0dacc1a83698768ac1f10c8d642a
reidkolaczek <mrkolaczek@crimson.ua.edu>
2016-06-16 18:50:48 -0500

    added login features.

--- config/passport.js
+++ config/passport.js
@@ -6,12 +6,6 @@ var User            = require('../app/models/user');
 // expose this function to our app using module.exports
 module.exports = function(passport) {
 
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
-
     // used to serialize the user for the session
     passport.serializeUser(function(user, done) {
         done(null, user.id);
@@ -23,12 +17,6 @@ module.exports = function(passport) {
             done(err, user);
         });
     });
-
-    // =========================================================================
-    // LOCAL SIGNUP ============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
 
     passport.use('local-signup', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
@@ -101,4 +89,6 @@ module.exports = function(passport) {
         });
 
     }));
+
+    
 };
